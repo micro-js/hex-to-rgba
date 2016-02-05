e69a2365d4cdfb1daa8a840ebd112c4a616dffd4
Andrew Shaffer <andrew@weo.io>
2016-02-04 19:10:54 -0800

    handle hex shorthand properly

--- test/index.js
+++ test/index.js
@@ -13,5 +13,7 @@ test('should work', function (t) {
   t.deepEqual(hexToRgba('#FFEE12'), [0xFF, 0xEE, 0x12, 1])
   t.deepEqual(hexToRgba('FFEE12'), [0xFF, 0xEE, 0x12, 1])
   t.deepEqual(hexToRgba(0xFFEE12), [0xFF, 0xEE, 0x12, 1])
+  t.deepEqual(hexToRgba('#FFF'), [0xFF, 0xFF, 0xFF, 1])
+  t.deepEqual(hexToRgba('#123'), [0x11, 0x22, 0x33, 1])
   t.end()
 })
